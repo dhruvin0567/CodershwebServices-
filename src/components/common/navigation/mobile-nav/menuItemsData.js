23efc777708a5b6c7e3271acfa8ab7fae040e069
Codersh Web Services <project.codersh@gmail.com>
2025-02-20 19:04:27 +0530

    navbar changes

--- src/components/common/navigation/mobile-nav/menuItemsData.js
+++ src/components/common/navigation/mobile-nav/menuItemsData.js
@@ -1,175 +1,236 @@
 export const menuItemsData = [
-	{
-		title: "Demo",
-		url: "#",
-		submenu: [
-			{
-				title: "Design Agency",
-				url: "/",
-			},
-			{
-				title: "Startup Agency",
-				url: "home-two",
-			},
-			{
-				title: "SEO Agency",
-				url: "home-three",
-			},
-			{
-				title: "Business Consultation",
-				url: "home-four",
-			},
-			{
-				title: "Digital Marketing",
-				url: "home-five",
-			},
-			{
-				title: "Interior Design Agency",
-				url: "home-six",
-			},
-			{
-				title: "Advertising agency",
-				url: "home-seven",
-			},
-		],
-	},
+	// {
+	// 	title: "Demo",
+	// 	url: "#",
+	// 	submenu: [
+	// 		{
+	// 			title: "Design Agency",
+	// 			url: "/",
+	// 		},
+	// 		{
+	// 			title: "Startup Agency",
+	// 			url: "home-two",
+	// 		},
+	// 		{
+	// 			title: "SEO Agency",
+	// 			url: "home-three",
+	// 		},
+	// 		{
+	// 			title: "Business Consultation",
+	// 			url: "home-four",
+	// 		},
+	// 		{
+	// 			title: "Digital Marketing",
+	// 			url: "home-five",
+	// 		},
+	// 		{
+	// 			title: "Interior Design Agency",
+	// 			url: "home-six",
+	// 		},
+	// 		{
+	// 			title: "Advertising agency",
+	// 			url: "home-seven",
+	// 		},
+	// 	],
+	// },
 	{
 		title: "About Us",
 		url: "about-us",
 	},
 	{
-		title: "Pages",
+		title: "Services",
 		url: "#",
 		submenu: [
 			{
-				title: "About Us",
-				url: "about-us",
+				title: "Services",
+				url: "service",
 			},
+			// {
+			// 	title: "Blog Grid",
+			// 	url: "blog-grid",
+			// },
+			// {
+			// 	title: "Blog Details",
+			// 	url: "single-blog",
+			// },
+		],
+	},
+	{
+		title: "Projects",
+		url: "#",
+		submenu: [
+			{
+				title: "Case Studies",
+				url: "casestudies",
+			},
+			{
+				title: "Our Work",
+				url: "ourwork",
+			},
+			// {
+			// 	title: "Blog Details",
+			// 	url: "single-blog",
+			// },
+		],
+	},
+	{
+		title: "Pages",
+		url: "#",
+		submenu: [
+
 			{
 				title: "Pricing",
 				url: "pricing",
 			},
 			{
-				title: "Blog",
-				url: "#",
-				submenu: [
-					{
-						title: "Our Blog",
-						url: "blog",
-					},
-					{
-						title: "Blog Grid",
-						url: "blog-grid",
-					},
-					{
-						title: "Blog Details",
-						url: "single-blog",
-					},
-				],
-			},
-			{
-				title: "Service",
-				url: "#",
-				submenu: [
-					{
-						title: "Service",
-						url: "service",
-					},
-					{
-						title: "Service Details",
-						url: "single-service",
-					},
-				],
+				title: "Brand",
+				url: "brand",
 			},
 			{
 				title: "Team",
-				url: "#",
-				submenu: [
-					{
-						title: "Team",
-						url: "team",
-					},
-					{
-						title: "Team Details",
-						url: "single-team",
-					},
-				],
+				url: "team",
 			},
 			{
 				title: "Portfolio",
-				url: "#",
-				submenu: [
-					{
-						title: "Portfolio One Column",
-						url: "portfolio-one",
-					},
-					{
-						title: "Portfolio Two Column",
-						url: "portfolio-two",
-					},
-					{
-						title: "Single Portfolio",
-						url: "single-portfolio",
-					},
-				],
+				url: "portfolio",
 			},
 			{
-				title: "Utility",
-				url: "#",
-				submenu: [
-					{
-						title: "Faq",
-						url: "faq",
-					},
-					{
-						title: "Error 404",
-						url: "error-page",
-					},
-					{
-						title: "Testimonial",
-						url: "testimonial",
-					},
-					{
-						title: "Coming Soon",
-						url: "coming-soon",
-					},
-				],
+				title: "Blog",
+				url: "blog1",
 			},
 			{
-				title: "Account",
-				url: "#",
-				submenu: [
-					{
-						title: "Sign Up",
-						url: "sign-up",
-					},
-					{
-						title: "Sign In",
-						url: "sign-in",
-					},
-					{
-						title: "Reset Password",
-						url: "reset-password",
-					},
-				],
+				title: "Testimonial",
+				url: "testimonial",
 			},
+			{
+				title: "FAQ",
+				url: "faq",
+			},
+			// {
+			// 	title: "Blog",
+			// 	url: "#",
+			// 	submenu: [
+			// 		{
+			// 			title: "Our Blog",
+			// 			url: "blog",
+			// 		},
+			// 		{
+			// 			title: "Blog Grid",
+			// 			url: "blog-grid",
+			// 		},
+			// 		{
+			// 			title: "Blog Details",
+			// 			url: "single-blog",
+			// 		},
+			// 	],
+			// },
+			// {
+			// 	title: "Service",
+			// 	url: "#",
+			// 	submenu: [
+			// 		{
+			// 			title: "Service",
+			// 			url: "service",
+			// 		},
+			// 		{
+			// 			title: "Service Details",
+			// 			url: "single-service",
+			// 		},
+			// 	],
+			// },
+			// {
+			// 	title: "Team",
+			// 	url: "#",
+			// 	submenu: [
+			// 		{
+			// 			title: "Team",
+			// 			url: "team",
+			// 		},
+			// 		{
+			// 			title: "Team Details",
+			// 			url: "single-team",
+			// 		},
+			// 	],
+			// },
+			// {
+			// 	title: "Portfolio",
+			// 	url: "#",
+			// 	submenu: [
+			// 		{
+			// 			title: "Portfolio One Column",
+			// 			url: "portfolio-one",
+			// 		},
+			// 		{
+			// 			title: "Portfolio Two Column",
+			// 			url: "portfolio-two",
+			// 		},
+			// 		{
+			// 			title: "Single Portfolio",
+			// 			url: "single-portfolio",
+			// 		},
+			// 	],
+			// },
+			// {
+			// 	title: "Utility",
+			// 	url: "#",
+			// 	submenu: [
+			// 		{
+			// 			title: "Faq",
+			// 			url: "faq",
+			// 		},
+			// 		{
+			// 			title: "Error 404",
+			// 			url: "error-page",
+			// 		},
+			// 		{
+			// 			title: "Testimonial",
+			// 			url: "testimonial",
+			// 		},
+			// 		{
+			// 			title: "Coming Soon",
+			// 			url: "coming-soon",
+			// 		},
+			// 	],
+			// },
+			// {
+			// 	title: "Account",
+			// 	url: "#",
+			// 	submenu: [
+			// 		{
+			// 			title: "Sign Up",
+			// 			url: "sign-up",
+			// 		},
+			// 		{
+			// 			title: "Sign In",
+			// 			url: "sign-in",
+			// 		},
+			// 		{
+			// 			title: "Reset Password",
+			// 			url: "reset-password",
+			// 		},
+			// 	],
+			// },
 		],
 	},
 	{
-		title: "Blog",
-		url: "blog",
+		title: "Solutions",
+		url: "#",
 		submenu: [
 			{
-				title: "Blog",
-				url: "blog",
+				title: "Credit Repair",
+				url: "creditrepair",
 			},
 			{
-				title: "Blog Grid",
-				url: "blog-grid",
+				title: "It Digital Agency",
+				url: "itdigitalagency",
 			},
 			{
-				title: "Blog Details",
-				url: "single-blog",
+				title: "Wine Store",
+				url: "winestore",
+			},
+			{
+				title: "Business Directory",
+				url: "business directory",
 			},
 		],
 	},
