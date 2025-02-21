8c7a3b7d5e55e0f35297d08f1fcad5639288ee8a
Codersh Web Services <project.codersh@gmail.com>
2025-02-21 12:51:45 +0530

    brand page public folder trial2

--- src/page/Brand.jsx
+++ src/page/Brand.jsx
@@ -1,16 +1,12 @@
-import BreadCrumb from "../components/common/Breadcrumb"
+import BreadCrumb from "../components/common/Breadcrumb";
 import { Link } from 'react-router-dom';
 
-
-
 function Brand() {
-
     const brandImages = [
         "1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png",
         "9.png", "10.png", "11.png", "12.png", "13.png", "14.png", "15.png",
         "16.png", "17.png", "18.png", "19.png", "20.png"
     ];
-
 
     return (
         <div>
@@ -22,17 +18,15 @@ function Brand() {
                         {brandImages.map((image, index) => (
                             <div className="col-lg-3 col-sm-4 col-6 mb-3" key={index}>
                                 <Link className="brand-card" to="#">
-                                    <img src={`/public/images/${image}`} alt={`brand ${index + 1}`} />
+                                    <img src={`/images/${image}`} alt={`brand ${index + 1}`} />
                                 </Link>
                             </div>
                         ))}
                     </div>
                 </div>
             </div>
-
-
         </div>
-    )
+    );
 }
 
-export default Brand
+export default Brand;
