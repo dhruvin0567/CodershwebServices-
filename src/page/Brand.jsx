cd6846ff26c3739ec10ab4184165f702ff77974d
Codersh Web Services <project.codersh@gmail.com>
2025-02-21 12:06:49 +0530

    blog page test

--- src/page/Brand.jsx
+++ src/page/Brand.jsx
@@ -1,56 +1,146 @@
+import BreadCrumb from "../components/common/Breadcrumb"
+import Img1 from '../assets/images/images2/1.png';
+import Img2 from '../assets/images/images2/2.png';
+import Img3 from '../assets/images/images2/3.png';
+import Img4 from '../assets/images/images2/4.png';
+import Img5 from '../assets/images/images2/5.png';
+import Img6 from '../assets/images/images2/6.png';
+import Img7 from '../assets/images/images2/7.png';
+import Img8 from '../assets/images/images2/8.png';
+import Img9 from '../assets/images/images2/9.png';
+import Img10 from '../assets/images/images2/10.png';
+import Img11 from '../assets/images/images2/11.png';
+import Img12 from '../assets/images/images2/12.png';
+import Img13 from '../assets/images/images2/13.png';
+import Img14 from '../assets/images/images2/14.png';
+import Img15 from '../assets/images/images2/15.png';
+import Img16 from '../assets/images/images2/16.png';
+import Img17 from '../assets/images/images2/17.png';
+import Img18 from '../assets/images/images2/18.png';
+import Img19 from '../assets/images/images2/19.png';
+import Img20 from '../assets/images/images2/20.png';
 
-import BreadCrumb from "../components/common/Breadcrumb";
-import { Link } from 'react-router-dom';
 
-// Import all the images statically
-import img1 from '../assets/Images/images2/1.png';
-import img2 from '../assets/Images/images2/2.png';
-import img3 from '../assets/Images/images2/3.png';
-import img4 from '../assets/Images/images2/4.png';
-import img5 from '../assets/Images/images2/5.png';
-import img6 from '../assets/Images/images2/6.png';
-import img7 from '../assets/Images/images2/7.png';
-import img8 from '../assets/Images/images2/8.png';
-import img9 from '../assets/Images/images2/9.png';
-import img10 from '../assets/Images/images2/10.png';
-import img11 from '../assets/Images/images2/11.png';
-import img12 from '../assets/Images/images2/12.png';
-import img13 from '../assets/Images/images2/13.png';
-import img14 from '../assets/Images/images2/14.png';
-import img15 from '../assets/Images/images2/15.png';
-import img16 from '../assets/Images/images2/16.png';
-import img17 from '../assets/Images/images2/17.png';
-import img18 from '../assets/Images/images2/18.png';
-import img19 from '../assets/Images/images2/19.png';
-import img20 from '../assets/Images/images2/20.png';
 
 function Brand() {
-    // Array of imported image variables
-    const brandImages = [
-        img1, img2, img3, img4, img5, img6, img7, img8,
-        img9, img10, img11, img12, img13, img14, img15,
-        img16, img17, img18, img19, img20
-    ];
+
 
     return (
         <div>
             <BreadCrumb title="brand" />
 
+
             <div className="brands-section py-5 my-sm-5">
                 <div className="container-fluid">
                     <div className="row">
-                        {brandImages.map((image, index) => (
-                            <div className="col-lg-3 col-sm-4 col-6 mb-3" key={index}>
-                                <Link className="brand-card" to="#">
-                                    <img src={image} alt={`brand ${index + 1}`} />
-                                </Link>
-                            </div>
-                        ))}
+                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
+                            <a className="brand-card" href="#">
+                                <img src={Img1} alt />
+                            </a>
+                        </div>
+                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
+                            <a className="brand-card" href="#">
+                                <img src={Img2} alt />
+                            </a>
+                        </div>
+                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
+                            <a className="brand-card" href="#">
+                                <img src={Img3} alt />
+                            </a>
+                        </div>
+                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
+                            <a className="brand-card" href="#">
+                                <img src={Img4} alt />
+                            </a>
+                        </div>
+                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
+                            <a className="brand-card" href="#">
+                                <img src={Img5} alt />
+                            </a>
+                        </div>
+                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
+                            <a className="brand-card" href="#">
+                                <img src={Img6} alt />
+                            </a>
+                        </div>
+                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
+                            <a className="brand-card" href="#">
+                                <img src={Img7} alt />
+                            </a>
+                        </div>
+                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
+                            <a className="brand-card" href="#">
+                                <img src={Img8} alt />
+                            </a>
+                        </div>
+                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
+                            <a className="brand-card" href="#">
+                                <img src={Img9} alt />
+                            </a>
+                        </div>
+                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
+                            <a className="brand-card" href="#">
+                                <img src={Img10} alt />
+                            </a>
+                        </div>
+                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
+                            <a className="brand-card" href="#">
+                                <img src={Img11} alt />
+                            </a>
+                        </div>
+                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
+                            <a className="brand-card" href="#">
+                                <img src={Img12} alt />
+                            </a>
+                        </div>
+                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
+                            <a className="brand-card" href="#">
+                                <img src={Img13} alt />
+                            </a>
+                        </div>
+                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
+                            <a className="brand-card" href="#">
+                                <img src={Img14} alt />
+                            </a>
+                        </div>
+                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
+                            <a className="brand-card" href="#">
+                                <img src={Img15} alt />
+                            </a>
+                        </div>
+                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
+                            <a className="brand-card" href="#">
+                                <img src={Img16} alt />
+                            </a>
+                        </div>
+                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
+                            <a className="brand-card" href="#">
+                                <img src={Img17} alt />
+                            </a>
+                        </div>
+                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
+                            <a className="brand-card" href="#">
+                                <img src={Img18} alt />
+                            </a>
+                        </div>
+                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
+                            <a className="brand-card" href="#">
+                                <img src={Img19} alt />
+                            </a>
+                        </div>
+                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
+                            <a className="brand-card" href="#">
+                                <img src={Img20} alt />
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
+
+
+
         </div>
-    );
+    )
 }
 
-export default Brand;
+export default Brand
