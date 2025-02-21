b56c53b267c6760fb3758262f066d9462b0ad6de
Codersh Web Services <project.codersh@gmail.com>
2025-02-21 16:42:24 +0530

    service page1 done

--- src/page/service/Pestrashop.jsx
+++ src/page/service/Pestrashop.jsx
@@ -1,6 +1,32 @@
 
 import BreadCrumb from '../../components/common/Breadcrumb'
+import Logo from '../../assets/images/images2/clutch.svg'
+import StarImg from '../../assets/images/images2/star.png'
 
+import ServiceIcon1 from '../../assets/images/images2/cms-setup.png'
+import ServiceIcon2 from '../../assets/images/images2/psd-to-wordpress.png'
+import ServiceIcon3 from '../../assets/images/images2/web-custom-development.png'
+import ServiceIcon4 from '../../assets/images/images2/web-development.png'
+import ServiceIcon5 from '../../assets/images/images2/plugin.png'
+import ServiceIcon6 from '../../assets/images/images2/maintenance.png'
+import ServiceIcon7 from '../../assets/images/images2/woo-commerce.png'
+import ServiceIcon8 from '../../assets/images/images2/blog.png'
+import ServiceIcon9 from '../../assets/images/images2/api.png'
+
+import Imgwithtxt1 from '../../assets/images/images2/Prestashop-1.png'
+import Imgwithtxt2 from '../../assets/images/images2/Prestashop-2.png'
+
+import ProcessCard1 from '../../assets/images/images2/reauirement-gathering.png'
+import ProcessCard2 from '../../assets/images/images2/uiux.png'
+import ProcessCard3 from '../../assets/images/images2/prototype.png'
+import ProcessCard4 from '../../assets/images/images2/dynamic-development.png'
+import ProcessCard5 from '../../assets/images/images2/testing.png'
+import ProcessCard6 from '../../assets/images/images2/live-development.png'
+import ProcessCard7 from '../../assets/images/images2/question.png'
+
+import Workcard1 from '../../assets/images/ourwork/New-Project-1-1024x1024.jpg'
+import Workcard2 from '../../assets/images/ourwork/New-Project-3-1024x1024.jpg'
+import Workcard3 from '../../assets/images/ourwork/New-Project-1024x1024.jpg'
 
 function Pestrashop() {
     return (
@@ -28,10 +54,10 @@ function Pestrashop() {
                             <a href="#" className="clutch-widget d-flex mb-4 mb-lg-0">
                                 <span className="me-2">
                                     <p className="mb-0">Reviewd On</p>
-                                    <img src="/src/assets/images/images2/clutch.svg" alt width={82} />
+                                    <img src={Logo} alt width={82} />
                                 </span>
                                 <span className="d-flex flex-column justify-content-between">
-                                    <img src="/src/assets/images/images2/star.png" alt width={90} />
+                                    <img src={StarImg} alt width={90} />
                                     <p className="review-ammount mb-0">16 Reviews</p>
                                 </span>
                             </a>
@@ -54,7 +80,7 @@ function Pestrashop() {
                         <div className="col-lg-4">
                             <div className="services-card">
                                 <div className="service-icon">
-                                    <img src="/src/assets/images/images2/cms-setup.png" alt />
+                                    <img src={ServiceIcon1} alt />
                                 </div>
                                 <h6 className="service-title">
                                     PrestaShop Store Setup
@@ -65,7 +91,7 @@ function Pestrashop() {
                         <div className="col-lg-4">
                             <div className="services-card">
                                 <div className="service-icon">
-                                    <img src="/src/assets/images/images2/psd-to-wordpress.png" alt />
+                                    <img src={ServiceIcon2} alt />
                                 </div>
                                 <h6 className="service-title">
                                     Precision Conversion from
@@ -76,7 +102,7 @@ function Pestrashop() {
                         <div className="col-lg-4">
                             <div className="services-card">
                                 <div className="service-icon">
-                                    <img src="/src/assets/images/images2/web-custom-development.png" alt />
+                                    <img src={ServiceIcon3} alt />
                                 </div>
                                 <h6 className="service-title">
                                     Custom PrestaShop
@@ -87,7 +113,7 @@ function Pestrashop() {
                         <div className="col-lg-4">
                             <div className="services-card">
                                 <div className="service-icon">
-                                    <img src="/src/assets/images/images2/web-development.png" alt />
+                                    <img src={ServiceIcon4} alt />
                                 </div>
                                 <h6 className="service-title">
                                     PrestaShop Theme Development &amp; Customization
@@ -97,7 +123,7 @@ function Pestrashop() {
                         <div className="col-lg-4">
                             <div className="services-card">
                                 <div className="service-icon">
-                                    <img src="/src/assets/images/images2/plugin.png" alt />
+                                    <img src={ServiceIcon5} alt />
                                 </div>
                                 <h6 className="service-title">
                                     PrestaShop Plugin Development Service
@@ -107,7 +133,7 @@ function Pestrashop() {
                         <div className="col-lg-4">
                             <div className="services-card">
                                 <div className="service-icon">
-                                    <img src="/src/assets/images/images2/maintenance.png" alt />
+                                    <img src={ServiceIcon6} alt />
                                 </div>
                                 <h6 className="service-title">
                                     PrestaShop Migration and
@@ -118,7 +144,7 @@ function Pestrashop() {
                         <div className="col-lg-4">
                             <div className="services-card">
                                 <div className="service-icon">
-                                    <img src="/src/assets/images/images2/woo-commerce.png" alt />
+                                    <img src={ServiceIcon7} alt />
                                 </div>
                                 <h6 className="service-title">
                                     PrestaShop Development
@@ -129,7 +155,7 @@ function Pestrashop() {
                         <div className="col-lg-4">
                             <div className="services-card">
                                 <div className="service-icon">
-                                    <img src="/src/assets/images/images2/blog.png" alt />
+                                    <img src={ServiceIcon8} alt />
                                 </div>
                                 <h6 className="service-title">
                                     PrestaShop Blog
@@ -140,7 +166,7 @@ function Pestrashop() {
                         <div className="col-lg-4">
                             <div className="services-card">
                                 <div className="service-icon">
-                                    <img src="/src/assets/images/images2/api.png" alt />
+                                    <img src={ServiceIcon9} alt />
                                 </div>
                                 <h6 className="service-title">
                                     PrestaShop API Integration
@@ -156,7 +182,7 @@ function Pestrashop() {
                 <div className="row m-0">
                     <div className="col-lg-6 p-0">
                         <div className="image-with-text-image">
-                            <img src="/src/assets/images/images2/Prestashop-1.png" alt />
+                            <img src={Imgwithtxt1} alt />
                         </div>
                     </div>
                     <div className="col-lg-6 p-0">
@@ -199,7 +225,7 @@ function Pestrashop() {
                 <div className="row m-0">
                     <div className="col-lg-6 p-0">
                         <div className="image-with-text-image">
-                            <img src="/src/assets/images/images2/Prestashop-2.png" alt />
+                            <img src={Imgwithtxt2} alt />
                         </div>
                     </div>
                     <div className="col-lg-6 p-0">
@@ -259,43 +285,43 @@ function Pestrashop() {
                     <div className="row">
                         <div className="col-lg-3">
                             <div className="process-card">
-                                <img src="/src/assets/images/images2/reauirement-gathering.png" alt />
+                                <img src={ProcessCard1} alt />
                                 <h6>1. Gathering requirements</h6>
                             </div>
                         </div>
                         <div className="col-lg-3">
                             <div className="process-card">
-                                <img src="/src/assets/images/images2/uiux.png" alt />
+                                <img src={ProcessCard2} alt />
                                 <h6>2. UI/UX Design</h6>
                             </div>
                         </div>
                         <div className="col-lg-3">
                             <div className="process-card">
-                                <img src="/src/assets/images/images2/prototype.png" alt />
+                                <img src={ProcessCard3} alt />
                                 <h6>3. Static Prototype</h6>
                             </div>
                         </div>
                         <div className="col-lg-3">
                             <div className="process-card">
-                                <img src="/src/assets/images/images2/dynamic-development.png" alt />
+                                <img src={ProcessCard4} alt />
                                 <h6>4. Dynamic Development</h6>
                             </div>
                         </div>
                         <div className="col-lg-4">
                             <div className="process-card">
-                                <img src="/src/assets/images/images2/testing.png" alt />
+                                <img src={ProcessCard5} alt />
                                 <h6>5. Quality Assurance</h6>
                             </div>
                         </div>
                         <div className="col-lg-4">
                             <div className="process-card">
-                                <img src="/src/assets/images/images2/live-development.png" alt />
+                                <img src={ProcessCard6} alt />
                                 <h6>6. Live Deployment </h6>
                             </div>
                         </div>
                         <div className="col-lg-4">
                             <div className="process-card">
-                                <img src="/src/assets/images/images2/question.png" alt />
+                                <img src={ProcessCard7} alt />
                                 <h6>7. Support and Upkeep</h6>
                             </div>
                         </div>
@@ -312,7 +338,7 @@ function Pestrashop() {
                     <div className="row">
                         <div className="col-lg-4 project" data-category="ecommerce-development">
                             <div className="work-card">
-                                <img src="/src/assets/images/ourwork/New-Project-1-1024x1024.jpg" alt className="work-image" />
+                                <img src={Workcard1} alt className="work-image" />
                                 <div className="about-work">
                                     <h5 className="work-name">Online Book Store</h5>
                                     <p className="work-technology">Shift4shop | React | Styled Components</p>
@@ -321,7 +347,7 @@ function Pestrashop() {
                         </div>
                         <div className="col-lg-4 project" data-category="web-development">
                             <div className="work-card">
-                                <img src="/src/assets/images/ourwork/New-Project-3-1024x1024.jpg" alt className="work-image" />
+                                <img src={Workcard2} alt className="work-image" />
                                 <div className="about-work">
                                     <h5 className="work-name">Texas Fowlers</h5>
                                     <p className="work-technology">Shift4shop | Bootstrap | JavaScript</p>
@@ -330,7 +356,7 @@ function Pestrashop() {
                         </div>
                         <div className="col-lg-4 project" data-category="ecommerce-development">
                             <div className="work-card">
-                                <img src="/src/assets/images/ourwork/New-Project-1024x1024.jpg" alt className="work-image" />
+                                <img src={Workcard3} alt className="work-image" />
                                 <div className="about-work">
                                     <h5 className="work-name">Redesign Hub</h5>
                                     <p className="work-technology">Wordpress | Elementor Pro | Calendly</p>
