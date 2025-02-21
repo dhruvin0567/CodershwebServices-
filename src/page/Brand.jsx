961662bbf17068031589535ab1ba3b1f8ab4176c
Codersh Web Services <project.codersh@gmail.com>
2025-02-21 12:47:50 +0530

    brand page public folder trial

--- src/page/Brand.jsx
+++ src/page/Brand.jsx
@@ -1,142 +1,34 @@
 import BreadCrumb from "../components/common/Breadcrumb"
-import Img1 from '../assets/images/images2/1.png';
-import Img2 from '../assets/images/images2/2.png';
-import Img3 from '../assets/images/images2/3.png';
-import Img4 from '../assets/images/images2/4.png';
-import Img5 from '../assets/images/images2/5.png';
-import Img6 from '../assets/images/images2/6.png';
-import Img7 from '../assets/images/images2/7.png';
-import Img8 from '../assets/images/images2/8.png';
-import Img9 from '../assets/images/images2/9.png';
-import Img10 from '../assets/images/images2/10.png';
-import Img11 from '../assets/images/images2/11.png';
-import Img12 from '../assets/images/images2/12.png';
-import Img13 from '../assets/images/images2/13.png';
-import Img14 from '../assets/images/images2/14.png';
-import Img15 from '../assets/images/images2/15.png';
-import Img16 from '../assets/images/images2/16.png';
-import Img17 from '../assets/images/images2/17.png';
-import Img18 from '../assets/images/images2/18.png';
-import Img19 from '../assets/images/images2/19.png';
-import Img20 from '../assets/images/images2/20.png';
+import { Link } from 'react-router-dom';
 
 
 
 function Brand() {
+
+    const brandImages = [
+        "1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png",
+        "9.png", "10.png", "11.png", "12.png", "13.png", "14.png", "15.png",
+        "16.png", "17.png", "18.png", "19.png", "20.png"
+    ];
 
 
     return (
         <div>
             <BreadCrumb title="brand" />
 
-
             <div className="brands-section py-5 my-sm-5">
                 <div className="container-fluid">
                     <div className="row">
-                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
-                            <a className="brand-card" href="#">
-                                <img src={Img1} alt />
-                            </a>
-                        </div>
-                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
-                            <a className="brand-card" href="#">
-                                <img src={Img2} alt />
-                            </a>
-                        </div>
-                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
-                            <a className="brand-card" href="#">
-                                <img src={Img3} alt />
-                            </a>
-                        </div>
-                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
-                            <a className="brand-card" href="#">
-                                <img src={Img4} alt />
-                            </a>
-                        </div>
-                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
-                            <a className="brand-card" href="#">
-                                <img src={Img5} alt />
-                            </a>
-                        </div>
-                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
-                            <a className="brand-card" href="#">
-                                <img src={Img6} alt />
-                            </a>
-                        </div>
-                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
-                            <a className="brand-card" href="#">
-                                <img src={Img7} alt />
-                            </a>
-                        </div>
-                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
-                            <a className="brand-card" href="#">
-                                <img src={Img8} alt />
-                            </a>
-                        </div>
-                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
-                            <a className="brand-card" href="#">
-                                <img src={Img9} alt />
-                            </a>
-                        </div>
-                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
-                            <a className="brand-card" href="#">
-                                <img src={Img10} alt />
-                            </a>
-                        </div>
-                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
-                            <a className="brand-card" href="#">
-                                <img src={Img11} alt />
-                            </a>
-                        </div>
-                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
-                            <a className="brand-card" href="#">
-                                <img src={Img12} alt />
-                            </a>
-                        </div>
-                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
-                            <a className="brand-card" href="#">
-                                <img src={Img13} alt />
-                            </a>
-                        </div>
-                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
-                            <a className="brand-card" href="#">
-                                <img src={Img14} alt />
-                            </a>
-                        </div>
-                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
-                            <a className="brand-card" href="#">
-                                <img src={Img15} alt />
-                            </a>
-                        </div>
-                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
-                            <a className="brand-card" href="#">
-                                <img src={Img16} alt />
-                            </a>
-                        </div>
-                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
-                            <a className="brand-card" href="#">
-                                <img src={Img17} alt />
-                            </a>
-                        </div>
-                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
-                            <a className="brand-card" href="#">
-                                <img src={Img18} alt />
-                            </a>
-                        </div>
-                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
-                            <a className="brand-card" href="#">
-                                <img src={Img19} alt />
-                            </a>
-                        </div>
-                        <div className="col-lg-3 col-sm-4 col-6 mb-3">
-                            <a className="brand-card" href="#">
-                                <img src={Img20} alt />
-                            </a>
-                        </div>
+                        {brandImages.map((image, index) => (
+                            <div className="col-lg-3 col-sm-4 col-6 mb-3" key={index}>
+                                <Link className="brand-card" to="#">
+                                    <img src={`/public/images/${image}`} alt={`brand ${index + 1}`} />
+                                </Link>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
-
 
 
         </div>
