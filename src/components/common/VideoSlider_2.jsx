c507fc9ca02b399ead85da9ca7a48050da299ee4
Project Codersh <project.codersh@gmail.com>
2025-03-19 17:18:22 +0530

    single brand page structure complete

---
+++ src/components/common/VideoSlider_2.jsx
@@ -0,0 +1,189 @@
+import video1 from '../../assets/images/Videos/Video-1-compressed.mp4';
+import video2 from '../../assets/images/Videos/Video-2-compressed.mp4';
+import video3 from '../../assets/images/Videos/Video-3-compressed.mp4';
+import video4 from '../../assets/images/Videos/Video-4-compressed.mp4';
+import video5 from '../../assets/images/Videos/Video-5-compressed.mp4';
+import video6 from '../../assets/images/Videos/Video-6-compressed.mp4';
+import { useState, useRef, useEffect } from 'react';  // Import Link from react-router-dom
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay } from 'swiper/modules';
+import FadeInStagger from "../animation/FadeInStagger";
+import '../../assets/assets/css/swiper-bundle.min.css'; // Import Swiper styles
+
+function VideoSlider_2() {
+
+    const videoFiles = [video1, video2, video3, video4, video5, video6];
+    const [currentVideo, setCurrentVideo] = useState(null);  // Track the currently playing video
+    const videoRefs = useRef([]); // Store references to video elements
+    const swiperRef = useRef(null); // Reference to Swiper instance
+
+    // Manually handle the "Play/Pause" of videos
+    const handlePlayPause = (index) => {
+        const videoElement = videoRefs.current[index];
+
+        if (currentVideo === index) {
+            // Pause the video if it's the currently playing one
+            videoElement.pause();
+            setCurrentVideo(null);
+        } else {
+            // Play the new video and pause the previous one
+            videoElement.play();
+            setCurrentVideo(index);
+            // Pause other videos
+            videoRefs.current.forEach((video, i) => {
+                if (i !== index) video.pause();
+            });
+        }
+    };
+
+    // Close video when clicked outside of the video container
+    const handleClickOutside = (e) => {
+        if (!e.target.closest('.video-wrap')) {
+            videoRefs.current.forEach((video) => video.pause());
+            setCurrentVideo(null);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('click', handleClickOutside);
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
+
+    // Custom navigation button handlers
+    const goToNextSlide = () => {
+        if (swiperRef.current) {
+            swiperRef.current.swiper.slideNext();
+        }
+    };
+    const goToPrevSlide = () => {
+        if (swiperRef.current) {
+            swiperRef.current.swiper.slidePrev();
+        }
+    };
+
+
+    return (
+        <div>
+            <FadeInStagger>
+                <div className="review-section text-center pt-5">
+                    <div className="container py-5">
+                        <div className='mb-5'>
+                            <h2>Our Client's Reviews</h2>
+                            <p className='brand-text'>
+                                We are very proud of the service we provide and stand by every product we carry. <br /> See our testimonials
+                                from our happy customers.
+                            </p>
+                        </div>
+                        <div className="slider-wrapper">
+                            <Swiper
+                                ref={swiperRef}
+                                spaceBetween={20}
+                                loop={true}
+                                slidesPerView={5}
+                                speed={1000}
+                                modules={[Navigation, Autoplay]}
+                                autoplay={{
+                                    delay: 3000, // Slide change delay in milliseconds
+                                    disableOnInteraction: false, // Autoplay will not be disabled after interactions
+                                }}
+                                breakpoints={{
+                                    320: { slidesPerView: 1 },
+                                    480: { slidesPerView: 1 },
+                                    768: { slidesPerView: 3 },
+                                    1024: { slidesPerView: 5 },
+                                }}
+                            >
+                                {videoFiles.map((video, index) => (
+                                    <SwiperSlide key={index}>
+
+                                        <button className="video-button" onClick={() => handlePlayPause(index)}>
+                                            <div className="video-wrap">
+                                                <video
+                                                    className="myVideo"
+                                                    width="100%" // Ensure the video takes up 100% of its slide space
+                                                    height="auto"
+                                                    ref={(el) => (videoRefs.current[index] = el)}
+                                                >
+                                                    <source
+                                                        src={video} // Use the imported video file here
+                                                        type="video/mp4"
+                                                    />
+                                                    Your browser does not support the video tag.
+                                                </video>
+                                                {/* Hide play button only for the currently playing video */}
+                                                {currentVideo !== index && (
+                                                    <span className="custom-play-button">▶</span>
+                                                )}
+                                            </div>
+                                        </button>
+
+                                    </SwiperSlide>
+                                ))}
+                            </Swiper>
+
+
+                            {/* Custom Navigation Buttons */}
+                            <div
+                                className="swiper-button-prev prevBtn"
+                                onClick={goToPrevSlide}
+                            >
+                                <svg
+                                    fill="#000000"
+                                    width="30"
+                                    height="30"
+                                    viewBox="0 0 24 24"
+                                    id="left-arrow"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="icon line"
+                                >
+                                    <path
+                                        id="primary"
+                                        d="M21,12H3M6,9,3,12l3,3"
+                                        style={{
+                                            fill: 'none',
+                                            stroke: 'rgb(0, 0, 0)',
+                                            strokeLinecap: 'round',
+                                            strokeLinejoin: 'round',
+                                            strokeWidth: 1.5,
+                                        }}
+                                    />
+                                </svg>
+                            </div>
+                            <div
+                                className="swiper-button-next nextBtn"
+                                onClick={goToNextSlide}
+                            >
+                                <svg
+                                    fill="#000000"
+                                    width="30"
+                                    height="30"
+                                    viewBox="0 0 24 24"
+                                    id="right-arrow"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="icon line"
+                                >
+                                    <path
+                                        id="primary"
+                                        d="M3,12H21M18,9l3,3-3,3"
+                                        style={{
+                                            fill: 'none',
+                                            stroke: 'rgb(0, 0, 0)',
+                                            strokeLinecap: 'round',
+                                            strokeLinejoin: 'round',
+                                            strokeWidth: 1.5,
+                                        }}
+                                    />
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </FadeInStagger>
+        </div>
+    )
+}
+
+export default VideoSlider_2
+
--- src/page/brand/SingleBrand.jsx
+++ src/page/brand/SingleBrand.jsx
@@ -1,49 +1,64 @@
 import BrandImg1 from "../../assets/images/brand/split-grip-1.png"
 import BrandImg2 from "../../assets/images/brand/split-grip-2.png"
 import BrandImg3 from "../../assets/images/brand/split-grip-3.png"
-import BrandImg4 from "../../assets/images/brand/Screenshot 2025-03-18 113553.png"
-
+import BrandImg4 from "../../assets/images/brand/split-grip-4.png"
 import Brandcard1 from "../../assets/images/images2/1.png"
 import Brandcard2 from "../../assets/images/images2/2.png"
 import Brandcard3 from "../../assets/images/images2/3.png"
+import VideoSlider_2 from "../../components/common/VideoSlider_2"
+
 
 
 function SingleBrand() {
     return (
-        <div>
+        <div style={{ overflowX: "hidden" }}>
+
+            <div className="Brand-breadcrumb">
+                <div className="container">
+                    <br />
+                    <br />
+                    <br />
+                </div>
+            </div>
 
             <div className="case-study-hero-section">
-                <img src={BrandImg1} alt />
-                {/* <div className="case-study-hero-section-img"></div> */}
+                <img src={BrandImg1} alt="Company_Site" className="img-fluid" />
             </div>
 
             <div className="cs-text">
                 <div className="container-lg ">
                     <div className="row">
                         <div className=" col-md-6 text-start">
-                            <div className="p-5">
+                            <div className="p-4 p-lg-5">
                                 <h2 className="pb-3">The Brief</h2>
                                 <p className="brand-text">
-                                    The Split Grip Trainer is a patent-pending tool designed to refine swing mechanics and boost player performance. Offering three training modes- split grip, short bat, and one-hand-it helps athletes develop strength, control, and efficiency, making it an essential training aid for improving batting technique.
-                                </p>
-                                <p className="brand-text">
-                                    Founded by former professional players Cory Voss and Trevor Goldberg, the Split Grip Trainer combines their extensive expertise in baseball and bat crafting. Cory, a Division 1 All-American and former draft pick, owns The Baseball Mill in Pueblo, Colorado, while Trevor, a former collegiate player, has been crafting custom wood bats through Berg Bat Company since the age of 15. Together, they developed the Split Grip Trainer to improve training and elevate player performance across all skill levels.
+                                    The Split Grip Trainer is a patent-pending tool designed to refine swing mechanics and enhance player performance. With three training modes-split grip, short bat and one-hand-it helps athletes build strength, control, and efficiency, making it an essential aid for improving batting technique. Founded by former professional players Cory Voss and Trevor Goldberg, the Split Grip Trainer combines their deep expertise in baseball and bat crafting. Cory, a Division 1 All-American and former draft pick, owns The Baseball Mill in Pueblo, Colorado, while Trevor, a former collegiate player, has been crafting custom wood bats with Berg Bat Company since he was 15. Together, they created the Split Grip Trainer to elevate training and player performance across all skill levels.
                                 </p>
                             </div>
                         </div>
                         <div className="col-md-6 px-5 bg-light">
-                            <ul className="py-5">
+                            <ul className="py-4 py-lg-5 mt-0 mt-md-5">
                                 <li className="brand-info-li">
                                     <p>Industry</p>
                                     <p className="fw-bold" style={{ marginBottom: "27px" }} >Sports Equipment</p>
                                 </li>
                                 <li className="brand-info-li">
                                     <p>Project</p>
-                                    <p className="fw-bold" style={{ marginBottom: "27px" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    <p className="fw-bold" style={{ marginBottom: "27px" }}>
+
+                                        Brand Design / Development
+                                        <br />
+                                        Web Design & Development
+                                    </p>
                                 </li>
                                 <li className="brand-info-li">
                                     <p>Technology</p>
-                                    <p className="fw-bold" style={{ marginBottom: "27px" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    <p className="fw-bold" style={{ marginBottom: "27px" }}>
+                                        Shopify Development <br />
+                                        E-Commerce & Payments Intigration <br />
+                                        Bootstrap UI Framework
+
+                                    </p>
                                 </li>
                                 <li className="brand-info-li">
                                     <p>Visit</p>
@@ -66,13 +81,13 @@ function SingleBrand() {
 
             <div className="image-section">
                 <div className="case-study-section-img">
-                    <img src={BrandImg2} alt />
+                    <img src={BrandImg2} alt="LaptopView_Of_Site" />
                 </div>
             </div>
 
             <div className="cs-richtext text-center">
-                <div className="container-lg bg-light p-5">
-                    <h2 className="mb-5">Challange</h2>
+                <div className="container-lg bg-light p-lg-5 p-sm-4 p-4">
+                    <h2 className="mb-4 mb-lg-5">Challange</h2>
                     <div className="cs-richtext-paragraph mx-lg-5 px-lg-5 pb-5">
                         <p className="brand-text" >
                             SplitGrip.com struggled with low user engagement and high cart abandonment rates, primarily due to a website structure that was difficult to navigate and an outdated design. The site’s user experience did not align with the premium brand image that SplitGrip aimed to project. As a result, customers found it hard to easily explore products, leading to frustration and lost sales. Additionally, the design lacked the modern, polished look needed to attract and retain customers. These issues hindered the company’s ability to fully capitalize on its innovative products and premium positioning in the market.
@@ -82,16 +97,16 @@ function SingleBrand() {
             </div>
 
             <div className="cs-images-section ">
-                <div className="container-lg p-5">
+                <div className="container-lg p-4 p-lg-5 p-sm-4">
                     <div className="row">
-                        <div className="col-md-6 ">
+                        <div className="col-6 ">
                             <div className="cs-img">
-                                <img src={BrandImg3} alt />
+                                <img src={BrandImg3} alt="LaptopView_Of_Site " />
                             </div>
                         </div>
-                        <div className="col-md-6 col-12 mt-sm-5 mt-5 mt-md-0">
+                        <div className="col-6">
                             <div className="cs-img">
-                                <img src={BrandImg4} alt />
+                                <img src={BrandImg4} alt="MobileView_Of_Site " />
                             </div>
                         </div>
                     </div>
@@ -99,45 +114,28 @@ function SingleBrand() {
             </div>
 
             <div className="cs-richtext text-center">
-                <div className="container-lg bg-light p-5">
-                    <h2 className="mb-5">Solution & Results</h2>
+                <div className="container-lg bg-light p-4 p-lg-5 p-sm-4">
+                    <h2 className="mb-4 mb-lg-5">Solution & Results</h2>
                     <div className="cs-richtext-paragraph mx-lg-5">
                         <p className="mb-3 brand-text">
-                            The team revamped SplitGrip’s WordPress WooCommerce store, focusing on creating a more user-friendly design with intuitive navigation. High-quality product images and strategically placed call-to-action buttons were integrated to drive conversions. These enhancements led to significant improvements:
-                        </p>
-                        <p className="mb-0 brand-text">
-                            average session duration increased by 40%
-                        </p>
-                        <p className="mb-0 brand-text">
-                            cart abandonment dropped by 35%
-                        </p>
-                        <p className=" brand-text">
-                            and revenue grew by 70% within just three months
-                        </p>
-                        <p className="mb-0  brand-text">
-                            The redesigned store not only improved the overall shopping experience but also aligned with the premium brand image, resulting in higher customer satisfaction and stronger sales performance.
+                            The team revamped SplitGrip’s WordPress WooCommerce store, prioritizing a user-friendly design with intuitive navigation. High-quality product images and strategically placed call-to-action buttons were integrated to drive conversions. These improvements led to notable results, including a 40% increase in average session duration, a 35% drop in cart abandonment, and a 70% growth in revenue within just three months. The redesigned store not only enhanced the overall shopping experience but also aligned with the premium brand image, resulting in higher customer satisfaction and a significant boost in sales performance.
                         </p>
                     </div>
                 </div>
             </div>
 
             <div>
-                <div className="container-lg cs-images-section-container px-lg-0">
+                <div className="container-lg cs-images-section-container px-0">
 
                     <div className="cs-image-section d-flex justify-content-center">
-                        <img src={BrandImg2} alt />
+                        <img src={BrandImg2} alt=" " />
                     </div>
 
                 </div>
                 {/* End */}
                 <div className="cs-video-section">
                     <div className="cs-video">
-                        <video
-                            src="/src/assets/images/brand/6222e2cd1b48d34290cc67b7ba43dc6c.mp4"
-                            controls
-                            style={{ width: '100%', height: '800px', objectFit: 'cover', objectPosition: "top" }}
-                        />
-
+                        <VideoSlider_2 />
                     </div>
                 </div>
             </div>
@@ -145,14 +143,14 @@ function SingleBrand() {
             <div className="image-cards-section py-5">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-4 my-3 my-sm-3 my-md-0">
-                            <img src={Brandcard1} style={{ borderRadius: "25px" }} alt />
+                        <div className="col-md-4 col-12 my-4 my-sm-4 my-md-auto ">
+                            <img src={Brandcard1} style={{ borderRadius: "25px" }} alt=" " />
                         </div>
-                        <div className="col-md-4 my-3 my-sm-3 my-md-0">
-                            <img src={Brandcard2} style={{ borderRadius: "25px" }} alt />
+                        <div className="col-md-4 col-12 my-4 my-sm-4 my-md-auto ">
+                            <img src={Brandcard2} style={{ borderRadius: "25px" }} alt=" " />
                         </div>
-                        <div className="col-md-4 my-3 my-sm-3 my-md-0">
-                            <img src={Brandcard3} style={{ borderRadius: "25px" }} alt />
+                        <div className="col-md-4 col-12 my-4 my-sm-4 my-md-auto">
+                            <img src={Brandcard3} style={{ borderRadius: "25px" }} alt=" " />
                         </div>
                     </div>
                 </div>
