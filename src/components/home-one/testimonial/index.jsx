caaea65cb26d911153aab487575dca1bf7c4ad82
Project Codersh <project.codersh@gmail.com>
2025-03-25 14:30:27 +0530

    imgs optmized

--- src/components/home-one/testimonial/index.jsx
+++ src/components/home-one/testimonial/index.jsx
@@ -1,3 +1,4 @@
+import FadeInStagger from "../../animation/FadeInStagger";
 import VideoSlider from "../../common/VideoSlider";
 import TestimonialSwiper from "./TestimonialSwiper";
 
@@ -5,7 +6,20 @@ function Testimonial() {
 	return (
 		<div>
 			<TestimonialSwiper />
-			<VideoSlider />
+			<FadeInStagger>
+				<div className="review-section text-center  aximo-section-padding6">
+					<div className="container">
+						<div>
+							<h3>Our Client's Reviews</h3>
+							<p>
+								We are very proud of the service we provide and stand by every product we carry.
+								See our testimonials from our happy customers.
+							</p>
+						</div>
+						<VideoSlider />
+					</div>
+				</div>
+			</FadeInStagger>
 		</div>
 	);
 }
