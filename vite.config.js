e856ae05b4c877316643d8835284e55aa00f6a99
Project Codersh <project.codersh@gmail.com>
2025-03-26 12:31:04 +0530

    poerformance optmization trial-1

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import VitePluginPurgeCss from 'vite-plugin-purgecss'
+import VitePluginImagemin from 'vite-plugin-imagemin'
+import VitePWA from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginPurgeCss(),
+    VitePluginImagemin(),
+    VitePWA()
+  ],
   assetsInclude: ['**/*.PNG']
 })
