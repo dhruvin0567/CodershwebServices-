afc95880ffb49f28510fe262097e0b1074feaee0
Project Codersh <project.codersh@gmail.com>
2025-03-26 12:36:59 +0530

    poerformance optmization trial-2

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import VitePluginPurgeCss from 'vite-plugin-purgecss'
 import VitePluginImagemin from 'vite-plugin-imagemin'
-import VitePWA from 'vite-plugin-pwa'
+import { VitePWA } from 'vite-plugin-pwa';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
