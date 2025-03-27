5cbdc03150bc56f08deb5ea007e2a1dbdef3f15e
Project Codersh <project.codersh@gmail.com>
2025-03-27 15:38:58 +0530

    optimization imrovement

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 // import VitePluginPurgeCss from 'vite-plugin-purgecss'
 import VitePluginImagemin from 'vite-plugin-imagemin'
 import { VitePWA } from 'vite-plugin-pwa';
+import ViteSitemap from 'vite-plugin-sitemap';
 
 
 // https://vitejs.dev/config/
@@ -11,7 +12,12 @@ export default defineConfig({
     react(),
     // VitePluginPurgeCss(),
     VitePluginImagemin(),
-    VitePWA()
+    VitePWA(),
+    ViteSitemap({
+      // The root URL of your site
+      hostname: 'https://codersh-web-services.vercel.app/',
+    })
   ],
   assetsInclude: ['**/*.PNG']
 })
+
