b47001713fcc6518af52a88c6c3ebdd07f0df4bd
Project Codersh <project.codersh@gmail.com>
2025-03-28 12:54:49 +0530

    homepage img slider test3

--- src/components/home-one/hero/HeroThumbs.jsx
+++ src/components/home-one/hero/HeroThumbs.jsx
@@ -1,8 +1,8 @@
 // import { useState, useEffect } from 'react';
 import HeroThumbImg1 from "../../../assets/images/v1/hero-thumb1.webp";
-// import HeroThumbImg2 from "../../../../public/images/hero-thumb-2.webp";
-import HeroThumbImg3 from "../../../../public/images/hero-thumb-3.webp";
-import HeroThumbImg4 from "../../../../public/images/hero-thumb-4.webp";
+import HeroThumbImg2 from "../../../assets/images/v1/icon/hero-thumb-2.webp";
+import HeroThumbImg3 from "../../../assets/images/v1/icon/hero-thumb-3.webp";
+import HeroThumbImg4 from "../../../assets/images/v1/icon/hero-thumb-4.webp";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from 'swiper/modules';
 
@@ -44,7 +44,7 @@ function HeroThumbs() {
 					<img src={HeroThumbImg1} alt="HeroImg" />
 				</SwiperSlide>
 				<SwiperSlide>
-					<img src="/images/hero-thumb-2.webp" alt="HeroImg" />
+					<img src={HeroThumbImg2} alt="HeroImg" />
 				</SwiperSlide>
 				<SwiperSlide>
 					<img src={HeroThumbImg3} alt="HeroImg" />
