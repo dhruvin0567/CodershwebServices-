a936bc6c4e2f29d21a345f589bc9cab83300a3db
Project Codersh <project.codersh@gmail.com>
2025-03-31 10:46:53 +0530

    trailing slash

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import VitePluginImagemin from 'vite-plugin-imagemin'
 import { VitePWA } from 'vite-plugin-pwa';
 import ViteSitemap from 'vite-plugin-sitemap';
 import SitemapPlugin from 'vite-plugin-sitemap';
-
+import Pages from 'vite-plugin-pages';
 
 
 // https://vitejs.dev/config/
@@ -23,6 +23,15 @@ export default defineConfig({
       baseUrl: 'https://codersh-web-services.vercel.app/',
       changefreq: 'daily',
       priority: 1.0
+    }),
+    Pages({
+      // Option to ensure trailing slashes
+      extendRoute(route) {
+        if (route.path && !route.path.endsWith('/')) {
+          route.path += '/'; // Add a trailing slash if not present
+        }
+        return route;
+      },
     })
   ],
   assetsInclude: ['**/*.PNG']
